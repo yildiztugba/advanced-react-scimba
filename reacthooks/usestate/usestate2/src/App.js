2735a99af9eba6231298ef9a6fa6da0d386fda0a
yildiztugba <okra.yildiz@gmail.com>
2023-05-10 16:56:34 +0200

    store data to state via onChange

--- reacthooks/usestate/usestate2/src/App.js
+++ reacthooks/usestate/usestate2/src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
-
+import React, {useState} from "react"
+import ReactDom from "react-dom"
 function App() {
+
+  const [inputData, setInputData] = useState({firstName: "",lastName:"" })
+  const [contactInfo, setContactInfo] = useState([])
+
+  function handleSubmit(event){
+
+  }
+
+  function handleClick(event){
+      const {name,value}= event.target
+
+      setInputData((prev)=>
+        ({...prev,
+        [name]: value})
+      )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-  
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="First Name"
+          name="firstName"
+          value={inputData.firstName}
+          onChange={handleClick}
+        />
+          <input
+          placeholder="Last Name"
+          name="lastName"
+          value={inputData.lastName}
+          onChange={handleClick}
+        />
+        <br />
+        <button>Add contact</button>
+
+      </form>
     </div>
   );
 }
