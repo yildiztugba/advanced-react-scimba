bf3bde313dfaadba46088efef50f5ca6d9325af6
yildiztugba <okra.yildiz@gmail.com>
2023-05-10 10:32:46 +0200

    import Header component

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import Header from "./Header"
 class App extends React.Component {
   render(){
       return (
           <div className="App">
-        
+            <Header />
           </div>
         );
   }
