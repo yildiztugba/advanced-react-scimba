570f9cd39c7c2f8ea9f79539edece21d95cdeba2
yildiztugba <okra.yildiz@gmail.com>
2023-05-10 10:52:04 +0200

    import consumer

--- context/src/App.js
+++ context/src/App.js
@@ -2,23 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from "./Header"
+import {userContextConsumer} from "./userContext"
 class App extends React.Component {
   render(){
       return (
           <div className="App">
             <Header />
+          <userContextConsumer>
+               { (context)=>{
+                        <main>
+                          <input 
+                          type="text"
+                          name="username"
+                          placeholder="New username"
+                          value=""
+                          onChange=""
+                        />
+                          <button onClick="">Change username </button>
 
-            <main>
-                <input 
-                  type="text"
-                  name="username"
-                  placeholder="New username"
-                  value=""
-                  onChange=""
-                />
-                <button onClick="">Change username </button>
-
-            </main>
+                          </main>
+                }}
+                      
+                      </userContextConsumer>
           </div>
         );
   }
