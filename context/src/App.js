d5d399bc3f14160937507c420cf986e34eb2656e
yildiztugba <okra.yildiz@gmail.com>
2023-05-10 10:31:50 +0200

    switching app to  class component from function

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends React.Component {
+  render(){
+      return (
+          <div className="App">
+        
+          </div>
+        );
+  }
 }
 
 export default App;
