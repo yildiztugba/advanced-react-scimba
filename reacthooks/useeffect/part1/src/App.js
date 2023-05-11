ac19dff76fd0325f4411ce1de834fac26f46873c
yildiztugba <okra.yildiz@gmail.com>
2023-05-11 09:33:47 +0200

    useeffect with randomcolor

--- reacthooks/useeffect/part1/src/App.js
+++ reacthooks/useeffect/part1/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{useEffect, useState} from "react"
+import randomColor from 'randomcolor';
+
 
 function App() {
+
+    const [count, setCount] = useState(0)
+    const [color, setColor]= useState("")
+
+    function increment(){
+      setCount(prev=>prev+1)
+    }
+    function decrement(){
+      setCount(prev=>prev-1)
+    }
+
+    useEffect(()=>{
+      setColor(randomColor())
+    },[count])
+
   return (
     <div className="App">
-      
+      <h1 style={{color:color}}>{count}</h1>
+      <button onClick={increment}>Increment</button>
+       <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
